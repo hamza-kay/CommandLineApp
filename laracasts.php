8a9940c764ff3a52c0655f2d3d11507b438ba5e8
hamza-kay <hamzahk1991@gmail.com>
2018-11-20 22:23:13 +0000

    Formatting

--- laracasts.php
+++ laracasts.php
@@ -3,11 +3,9 @@
 <?php
 
 
-
+use Acme\SayHelloCommand;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
+
 
 
 require 'vendor/autoload.php';
@@ -15,21 +13,6 @@ require 'vendor/autoload.php';
 
 $app = new Application('Hamza Demo 1.0');
 
-$app->register('sayHelloTo')
-    ->setDescription('Offer a greeting to the given person.')
-    ->addArgument('name', InputArgument::OPTIONAL, 'Your Name.')
-    ->setCode(function(InputInterface $input, OutputInterface $output)
-    {
-
-
-        $message = ('Hello ' . $input->getArgument('name'));
-
-        $output->writeln("<info>{$message}</info>");
-
-    });
-
-
-
-
+$app->add(new SayHelloCommand);
 
 $app->run();
